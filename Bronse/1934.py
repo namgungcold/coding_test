818223873e2d0ab860755c2be1378df40795db7d
namgungcold <105970056+namgungcold@users.noreply.github.com>
2024-10-09 20:03:08 +0900

    bronse1

---
+++ Bronse/1934.py
@@ -0,0 +1,8 @@
+num = int(input())
+for j in range(num):
+    a,b = map(int,input().split())
+    for i in range(1,min(a,b)+1):
+        if a%i==0 and b%i==0:
+            gcd = i
+    lcm = a*b//gcd
+    print(lcm)
